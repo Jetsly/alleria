e979fceaf66377716bc288c6c8399ce15915a7be
Jetsly <cnljli@live.com>
2019-07-17 10:25:43 +0800

    fix: move github

--- bin/deploy-gh-page.sh
+++ bin/deploy-gh-page.sh
@@ -2,10 +2,10 @@
 set -e # exit with nonzero exit code if anything fails
 
 project=`git remote  get-url origin | awk -F ":" '{print $2}'`
+project=${project/*github.com\//}
 echo "deploy $1 file to github<$project> page"
-if [ -f "CNAME" ]; then
-  cp CNAME $1 
-fi
+
+
 if [ ! ${GITHUB_TOKEN} ]; then
   echo ${GITHUB_TOKEN?Not Found}
   exit 1
@@ -14,6 +14,12 @@ if [ ! -d "$1" ]; then
   echo  "$1 not exist"
   exit 1
 fi
+if [ -f "CNAME" ]; then
+  cp CNAME $1 
+fi
+
+echo "Starting to update gh-pages\n"
+
 cd $1
 git init
 git config --global user.email "travis@travis-ci.org"
@@ -22,3 +28,4 @@ git add -f .
 git commit -m "Travis build"
 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/$project" master:gh-pages > /dev/null
 
+echo "Done updating gh-pages\n"
